10446b84f38012c1e6e3333e7e64eb7993972504
soonn22 <71654147+soonn22@users.noreply.github.com>
2022-11-16 12:32:46 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## hilbert function makes a square matrix, taking size of matrix. If hilbert(2) 
+## entered, then make 2 by 2 square matrix. when 5 entered, then make 5 by 5 matrix.
+## makeCahceMatrix is to store matrix and return matrix
+## cacheSolve check there is inverse matrix is in there already or not. if it has, then 
+## return it. if it does not have it, then make a inverse matrix by using solve function
 
 ## Write a short comment describing this function
+hilbert <- function(n) { i <- 1:n; 1 / outer(i - 1, i, `+`) }
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setMatrix <- function(matrix) m <<- matrix
+  getMatrix <- function() m
+  list(set = set, get = get,
+       setMatrix = setMatrix,
+       getMatrix = getMatrix)
 }
+x <- hilbert(2)
 
+y <-matrix(rnorm(36),6,6)
 
 ## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getMatrix()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setMatrix(m)
+  m
+  ## Return a matrix that is the inverse of 'x'
 }
+
+cacheSolve(makeCacheMatrix(x))
+y <- makeCacheMatrix(x)
+cacheSolve(y)
+y
+x
